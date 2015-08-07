c0f5a3e935d0832d850deb50d81946ace5f5d3b4
Mika Braginsky <mikabr@stanford.edu>
2015-08-07 13:26:09 -0700

    applied hadley style to all code

--- R/solarized_palette.R
+++ R/solarized_palette.R
@@ -1,9 +1,9 @@
 #' solarized colour palette
-#' 
+#'  
 #' Provides a colour scheme based on the 
 #' \href{http://ethanschoonover.com/solarized}{solarized} accent colours.
 #' 
-#' @param num_values Number of values to colour. If more than 9, colours start
+#' @param num_values Number of values to colour. If more than 9, colours start 
 #'   to repeat.
 #'   
 #' @examples
@@ -12,29 +12,30 @@
 #'   show_col(solarized_palette(9))
 #' }
 solarized_palette <- function(num_values) {
-  
+
   solarized_colors <- c(magenta = "#d33682",
                         red = "#dc322f",
                         orange = "#cb4b16",
-                        yellow = "#b58900", 
+                        yellow = "#b58900",
                         green = "#859900",
                         cyan = "#2aa198",
                         blue = "#268bd2",
                         violet = "#6c71c4",
                         purple = "#993399")
-  
+
   color_order <- c("blue", "orange", "green", "purple", "magenta",
                    "yellow", "cyan", "violet", "red")
-  
+
   num_colors <- length(color_order)
+  num_color_values <- num_values %% num_colors
   palette <- c(rep(solarized_colors, num_values %/% num_colors),
                solarized_colors[Filter(
-                 function(color) color %in% color_order[0:(num_values %% num_colors)],
+                 function(color) color %in% color_order[0:num_color_values],
                  names(solarized_colors)
                )])
   names(palette) <- NULL
   return(palette)
-  
+
 }
 
 
@@ -53,15 +54,15 @@ solarized_palette <- function(num_values) {
 #'   geom_point() +
 #'   scale_colour_solarized()
 scale_colour_solarized <- function(...) {
-    ggplot2::discrete_scale("colour", "solarized", solarized_palette, ...)
+  ggplot2::discrete_scale("colour", "solarized", solarized_palette, ...)
 }
 
 #' @rdname scale_colour_solarized
 scale_color_solarized <- function(...) {
-    scale_colour_solarized(...)
+  scale_colour_solarized(...)
 }
 
 #' @rdname scale_colour_solarized
 scale_fill_solarized <- function(...) {
-    ggplot2::discrete_scale("fill", "solarized", solarized_palette, ...)
-} 
+  ggplot2::discrete_scale("fill", "solarized", solarized_palette, ...)
+}
