14c2d6f6a9e674c51cd1b0a6690bab1efff87f50
Mika Braginsky <mikabr@stanford.edu>
2015-10-07 21:24:48 -0700

    size argument in multi_boot

--- R/multiboot.R
+++ R/multiboot.R
@@ -1,17 +1,18 @@
 #' Non-parametric bootstrap for numeric vector data
-#' 
+#'
 #' Computes arbitrary bootstrap statistics on univariate data.
-#' 
+#'
 #' @param data A numeric vector of data to bootstrap over.
-#' @param summary_function A string that is the name of a function to be 
+#' @param summary_function A string that is the name of a function to be
 #'   computed over each set of samples. This function needs to take a vector and
 #'   return a single number (defaults to \code{"mean"}).
-#' @param statistics_functions A vector of strings that are names of functions to be 
+#' @param statistics_functions A vector of strings that are names of functions to be
 #'   computed over the set of summary values from all samples.
 #' @param nboot The number of bootstrap samples to take (defaults to \code{1000}).
+#' @param size The fraction of items to sample (defaults to 1).
 #' @param replace Logical indicating whether to sample with replacement (defaults to \code{TRUE}).
 #' @param ... Other arguments passed from generic.
-#' 
+#'
 #' @examples
 #' ## Mean and 95% confidence interval for 1000 samples from a normal distribution
 #' x <- rnorm(1000, mean = 0, sd = 1)
@@ -23,13 +24,15 @@ multi_boot.numeric <- function(data,
                                summary_function = "mean",
                                statistics_functions,
                                nboot = 1000,
+                               size = 1,
                                replace = TRUE, ...) {
 
   formulas <- sapply(statistics_functions,
                      function(x) lazyeval::interp(~fun, fun = x))
 
   one_sample <- function() {
-    do.call(summary_function, list(sample(data, replace = replace)))
+    do.call(summary_function, list(sample(data, size = size * length(data),
+                                          replace = replace)))
   }
 
   all_samples <- data.frame(sample = replicate(nboot, one_sample())) %>%
@@ -45,12 +48,12 @@ multi_boot.numeric <- function(data,
 }
 
 #' Non-parametric bootstrap for logical vector data
-#' 
+#'
 #' Computes arbitrary bootstrap statistics on univariate data.
-#' 
+#'
 #' @param data A logical vector of data to bootstrap over.
 #' @inheritParams multi_boot.numeric
-#'   
+#'
 #' @examples
 #' ## Mean and 95% confidence interval for 1000 samples from a binomial distribution
 #' x <- as.logical(rbinom(1000, 1, 0.5))
@@ -62,15 +65,16 @@ multi_boot.logical <- function(data,
                                summary_function = "mean",
                                statistics_functions,
                                nboot = 1000,
+                               size = 1,
                                replace = TRUE, ...) {
   multi_boot(as.numeric(data), summary_function, statistics_functions,
-             nboot, replace, ...)
+             size, nboot, replace, ...)
 }
 
 #' Non-parametric bootstrap for data frames
-#' 
+#'
 #' Computes arbitrary bootstrap statistics on univariate data.
-#' 
+#'
 #' @param data A data frame.
 #' @param summary_function A function to be computed over each set of samples as a data frame, or a
 #'   string that is the name of the function to be computed over each set of samples as a single column of
@@ -85,9 +89,10 @@ multi_boot.logical <- function(data,
 #' @param statistics_groups A vector of strings that are column names of \code{data} indicating how it should
 #'   be grouped before applying \code{statistics_functions} (defaults to \code{summary_groups}).
 #' @param nboot The number of bootstrap samples to take (defaults to \code{1000}).
+#' @param size The fraction of rows to sample (defaults to 1).
 #' @param replace Logical indicating whether to sample with replacement (defaults to \code{TRUE}).
 #' @param ... Other arguments passed from generic.
-#'  
+#'
 #' @examples
 #' ## Mean and 95% confidence interval for 1000 samples from two different normal distributions
 #' require(dplyr)
@@ -102,7 +107,7 @@ multi_boot.logical <- function(data,
 #'            summary_function = function(df) summarise(df, mean = mean(value)),
 #'            summary_groups = c("condition"),
 #'            statistics_functions = function(df) summarise_each(df,
-#'                                                               funs("ci_upper", "mean", "ci_lower"), 
+#'                                                               funs("ci_upper", "mean", "ci_lower"),
 #'                                                               mean),
 #'            statistics_groups = c("condition"),
 #'            nboot = 100, replace = TRUE)
@@ -114,6 +119,7 @@ multi_boot.data.frame <- function(data,
                                   statistics_functions,
                                   statistics_groups = summary_groups,
                                   nboot = 1000,
+                                  size = 1,
                                   replace = TRUE, ...) {
 
   fun_types <- c("closure", "character")
@@ -152,7 +158,7 @@ multi_boot.data.frame <- function(data,
           group_by_(.dots = summary_groups)
       }
       df %>%
-        sample_frac(replace = replace) %>%
+        sample_frac(size = size, replace = replace) %>%
         call_summary_function() %>%
         mutate(sample = k)
     }
@@ -184,21 +190,21 @@ multi_boot.data.frame <- function(data,
 
 #' Nonparemetric Bootstrap and Empirical central tendency for data frames
 #' Designed to make standard use of \code{multi_boot.data.frame} easier
-#' 
-#' Computes arbitrary bootstrap statistics on univariate data. 
+#'
+#' Computes arbitrary bootstrap statistics on univariate data.
 #' NOTE: Both empirical functions and bootstrapping functions will be computed
 #'  over the grouping variables currently specified for the data frame.
-#' 
+#'
 #' @param data A data frame.
-#' @param column A string indicating the column of \code{data} to bootstrap 
-#' @param na.rm A logical indicating whether NAs should be dropped before 
+#' @param column A string indicating the column of \code{data} to bootstrap
+#' @param na.rm A logical indicating whether NAs should be dropped before
 #'  bootstrapping (defaults to \code{FALSE})
-#' @param empirical_function a string indicating the function to compute and 
+#' @param empirical_function a string indicating the function to compute and
 #'  bootstrap over (defaults to \code{mean})
 #' @param statistics_functions A vector of strings that are names of functions
 #'  to be bootstrapped (defaults to \code{c("ci_lower", "ci_upper")})
 #' @param nboot The number of bootstrap samples to take (defaults to \code{1000}).
-#'  
+#'
 #' @examples
 #' ## Mean and 95% confidence interval for 1000 samples from two different normal distributions
 #' require(dplyr)
@@ -242,14 +248,14 @@ multi_boot_standard <- function(data, column, na.rm = FALSE,
 }
 
 #' Non-parametric bootstrap with multiple sample statistics
-#' 
+#'
 #' \code{multi_boot} is a generic function for bootstrapping on various data
 #' structures. The function invokes particular methods which depend on the class
 #' of the first argument.
-#' 
+#'
 #' @param data A data structure containg the data to bootstrap.
 #' @param ... Additional arguments passed to particular methods.
-#' 
+#'
 #' @examples
 #' ## List of available methods
 #' methods(multi_boot)
